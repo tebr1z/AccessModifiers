5d08eb8e3c9e9c20a80641e67cd0a4bf13125b9d
tebr1z <tabrizyh@code.edu.az>
2024-03-06 07:13:27 +0700

    Resolved the issue and task is ready.

---
+++ AccessModife/ChineCity.cs
@@ -0,0 +1,24 @@
+﻿
+namespace AccessModife
+{
+    
+
+    public class ChinaCity : City
+    {
+        public ChinaCity(string name, int population) : base(name, population)
+        {
+            if (population <= 100000)
+            {
+                Console.WriteLine("1.000.000 asagi deyer daxil etmeyin.");
+                return;
+            }
+            else
+            {
+                Name = name;
+                Population = population;
+            }
+        }
+    }
+
+
+}
---
+++ AccessModife/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AccessModife
+{
+    class Program
+    {
+        static double CalculateAveragePopulation(City[] cityArray)
+        {
+            int totPopulation = 0;
+            foreach (City city in cityArray)
+            {
+                totPopulation += city.Population;
+            }
+            return cityArray.Length > 0 ? (double)totPopulation / cityArray.Length : 0;
+        }
+
+        static void Main(string[] args)
+        {
+            City[] chinaCityArray = new City[3];
+
+            for (int i = 0; i < chinaCityArray.Length; i++)
+            {
+                string cityName;
+                int population;
+                bool validPopulation = false;
+
+                do
+                {
+                    Console.Write($"Sheher adini daxil edin {i + 1}: ");
+                    cityName = Console.ReadLine();
+
+                    Console.Write($"Sheher ehali sayni daxil edin{i + 1}: ");
+                    string populationInput = Console.ReadLine();
+
+                    if (!int.TryParse(populationInput, out population) || population <= 0)
+                    {
+                        Console.WriteLine("0 Ve Ya Daha asagi deyer daxil ede bilmezsiz");
+                    }
+                    else
+                    {
+                        validPopulation = true;
+                    }
+                } while (!validPopulation);
+
+                chinaCityArray[i] = new ChinaCity(cityName, population);
+            }
+
+            double averagePopulation = CalculateAveragePopulation(chinaCityArray);
+            Console.WriteLine("Ehali sayi: " + averagePopulation);
+        }
+    }
+}
