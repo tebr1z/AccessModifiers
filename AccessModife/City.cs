0cac421df732de30b6cf576d11a554b258ea327f
tebr1z <tabrizyh@code.edu.az>
2024-03-06 04:05:45 +0400

    Feat: City

---
+++ AccessModife/City.cs
@@ -0,0 +1,27 @@
+﻿
+namespace AccessModife
+{
+    public class City
+    {
+        private string name;
+        private int population;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Population
+        {
+            get { return population; }
+            set { population = value; }
+        }
+
+        public City(string name, int population)
+        {
+            Name = name;
+            Population = population;
+        }
+    }
+}
